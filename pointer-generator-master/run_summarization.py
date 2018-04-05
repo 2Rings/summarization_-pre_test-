15fcf679afc439cd85b9c8ce7ae8a9c19bcf2bb6
Harden <group2ringsfield@gmail.com>
2018-04-05 16:04:47 -0500

    fixed one bug#2

--- pointer-generator-master/run_summarization.py
+++ pointer-generator-master/run_summarization.py
@@ -294,9 +294,15 @@ def main(unused_argv):
   # Make a namedtuple hps, containing the values of the hyperparameters that the model needs
   hparam_list = ['mode', 'lr', 'adagrad_init_acc', 'rand_unif_init_mag', 'trunc_norm_init_std', 'max_grad_norm', 'hidden_dim', 'emb_dim', 'batch_size', 'max_dec_steps', 'max_enc_steps', 'coverage', 'cov_loss_wt', 'pointer_gen']
   hps_dict = {}
-  for key,val in FLAGS.__flags.iteritems(): # for each flag
-    if key in hparam_list: # if it's in the list
-      hps_dict[key] = val # add it to the dict
+
+  # for key,val in FLAGS.__flags.iteritems(): # for each flag
+  #   if key in hparam_list: # if it's in the list
+  #     hps_dict[key] = val # add it to the dict
+
+  for key in hparam_list:
+      if hasattr(FLAGS, key):
+          hps_dict[key] = getattr(FLAGS, key)
+          
   hps = namedtuple("HParams", hps_dict.keys())(**hps_dict)
 
   # Create a batcher object that will create minibatches of data
